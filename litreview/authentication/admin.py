3e07623df5153ae2ddc9e12bcbd06a357dec44bb
enrootmt <enrootmauvaisetroupe@gmail.com>
2025-02-05 16:39:07 +0100

    add project

---
+++ litreview/authentication/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+# listings/admin.py
+
+from django.contrib import admin
+
+from authentication.models import User
+
+admin.site.register(User)
